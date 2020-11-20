cd9e422ec095b8f6ae0b4514dbd8c832377c70a5
vihaanbhatt <73344051+vihaanbhatt@users.noreply.github.com>
2020-11-20 21:26:42 +0530

    Update CGSO.js

--- CGSO.js
+++ CGSO.js
@@ -19,9 +19,14 @@ function draw() {
   background(0);
   if(wall.x-car.x < (car.width+wall.width)/2)
   {
-    car.shapeColor=color(255, 0, 0);
+     car.velocityX=0;
+   var deformation=0.5 * weight * speed */22509;
 
   } 
+  if(deformation>180)
+  {
+  car.shapeColor=color(255,0,0)
+  }
   if(deformation<180 && deformation>100)
   {
     car.shapeColor=color(230, 230, 0);
