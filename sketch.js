15f04b76ba1664cb46b49a8cf7c18eb48cd81250
vihaanbhatt <73344051+vihaanbhatt@users.noreply.github.com>
2020-11-21 11:12:34 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,8 +10,8 @@ function setup(){
   car.shapeColor=color(255)
 
 
-wall=createSprite(1500, 200, 60, height/2)
-wall.shapeColor=color(lightgrey)
+wall=createSprite(500, 200, 60, height/2)
+wall.shapeColor=color(80,80,80)
 
 }
 
@@ -19,16 +19,22 @@ function draw() {
   background("grey");
   if(wall.x-car.x < (car.width+wall.width)/2)
   {
-    car.shapeColor=color(255, 0, 0);
-
-  } 
+   car.velocityX=0;
+   var deformation=0.5 * weight * speed *speed/22500; 
+  
+  if(deformation>180)
+  {
+    car.shapeColor=color(255,0,0)
+  }
   if(deformation<180 && deformation>100)
   {
-    car.shapeColor=color(230,230,0);
+    car.shapeColor=color(230, 230, 0);
+   
   } 
   if(deformation<100)
+ 
   {
-  car.shapeColor=color(0,255,0)
-}
+  car.shapeColor=color(0, 255, 0)
+}}
   drawSprites();
 }
